9701cc1793207d018a235630ca3d9da0c4966b88
Olivia Hanly (23331483) <23331483@student.uwa.edu.au>
2025-07-09 13:59:16 +0800

    Add change type modal

--- app/static/js/admin.js
+++ app/static/js/admin.js
@@ -2,10 +2,14 @@
 window.onload = function() {
 
     document.getElementById('UserType').addEventListener("change", selectedTypeChanged);
+    document.querySelectorAll('.changeTypeBtn').forEach( e => e.addEventListener("click", changeTypeBtnClicked));
+}
 
-    //document.getElementById('UserType').addEventListener("change", userTypeChanged);
-    //hideOrShowEmailAndSubmitContainer(document.getElementById('UserType').value);
-    //changeHeadings(document.getElementById('UserType').value);
+function changeTypeBtnClicked(e) {
+
+    userEmail = e.target.dataset.email;
+    userType = e.target.dataset.userType;
+    $('#changeTypeModal').modal('show');
 }
 
 function selectedTypeChanged(e) {
@@ -14,42 +18,6 @@ function selectedTypeChanged(e) {
     window.location.href = '/admin?selectedType=' + selectedUserType;
 }
 
-function userTypeChanged(e) {
-
-    selectedUserType = e.target.value;
-    changeHeadings(selectedUserType);
-    hideOrShowEmailAndSubmitContainer(selectedUserType);
-    removeErrors();
-}
-
-function hideOrShowEmailAndSubmitContainer(selectedUserType) {
-    let containerElement = document.getElementById("emailAndSubmitContainer");
-    
-    if (selectedUserType == "facilitator") {
-        containerElement.style.display = "none";
-    }
-    else {
-        containerElement.style.display = "block";
-    }
-}
-
-function removeErrors() {
-    const errorElements = document.querySelectorAll('.addUserFormError');
-    errorElements.forEach( e => e.remove());
-}
-
-function changeHeadings(selectedUserType) {
-    const headingSpanElements = document.querySelectorAll('.userTypeValue');
-
-    for (let span of headingSpanElements) {
-        if (selectedUserType == "admin") {
-            span.innerHTML = "Admin";
-        }
-        if (selectedUserType == "coordinator") {
-            span.innerHTML = "Coordinator";
-        }
-        if (selectedUserType == "facilitator") {
-            span.innerHTML = "Facilitator";
-        }
-    }
+function changeType() {
+    alert('changing type...');
 }
